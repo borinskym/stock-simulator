8f53fa0c729de3d18f53e5624d346b95b0594d1a
agur <amaziagur@gmail.com>
2016-12-14 17:51:08 +0200

    fix python script

--- docker_registry_discovery.py
+++ docker_registry_discovery.py
@@ -15,13 +15,19 @@ for line in f.readlines():
     splited = line.split(':')
     conf[splited[0]] = splited[1].strip()
 
+print "set aws_access_key_id" + sys.argv[1]
 os.system("aws configure set aws_access_key_id " + sys.argv[1])
+print "set aws_secret_access_key" + sys.argv[2]
 os.system("aws configure set aws_secret_access_key " + sys.argv[2])
+print "login"
 os.system("aws ecr get-login --region us-east-1")
+print "batch-delete-image"
 os.system("aws ecr --region us-east-1 batch-delete-image --repository-name hello-world-java --image-ids imageTag=0.1.0")
 #os.system("aws ecr --region us-east-1 batch-delete-image --repository-name" + conf['name']  + "--image-ids imageTag=" + conf['version'])
 
 retVal = registryUri + "/" + conf['name'] + ":" + conf['version']
+
+print "retVal -> " + retVal
 
 #print "push new version to registry"
 #os.system("docker push " + retVal)
