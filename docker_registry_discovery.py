c204013aac8d615ad51ec92fc5deed0a863d4831
agur <amaziagur@gmail.com>
2016-12-14 15:51:28 +0200

    fix python script

--- docker_registry_discovery.py
+++ docker_registry_discovery.py
@@ -1,15 +1,12 @@
-import sys
-
 f = open('config.yml', 'r')
 
-#registryUri = "911479539546.dkr.ecr.us-east-1.amazonaws.com" #hard coded for now
+registryUri = "911479539546.dkr.ecr.us-east-1.amazonaws.com" #hard coded for now
 
 conf = {}
 for line in f.readlines():
     splited = line.split(':')
     conf[splited[0]] = splited[1].strip()
 
-
-for arg in sys.argv: 1
-retVal = arg + "/" + conf['name'] + ":" + conf['version']
+retVal = registryUri + "/" + conf['name'] + ":" + conf['version']
 print retVal.strip() ,
+# sys.stdout.write(retVal)
