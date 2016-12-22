bec6f1ce6f721f069123753340ea8c8f63b3a2af
zvikai <zvika4@gmail.com>
2016-12-22 16:08:29 +0200

    bibibibi

--- service/src/main/java/com/agt/mes/helloworld/GreeterController.java
+++ service/src/main/java/com/agt/mes/helloworld/GreeterController.java
@@ -9,7 +9,7 @@ public class GreeterController {
 
     @RequestMapping("/greeting")
     public Greeting greeting() {
-        return new Greeting("Hello, La la");
+        return new Greeting("bibibibibibibibibib    Hello, La la");
     }
 
 }
