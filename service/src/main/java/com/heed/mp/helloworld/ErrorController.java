8d193117bb02f0077a99e2e6928e70fb4ce9c7d6
Eli Segal <esegal@agtinternational.com>
2018-02-03 08:14:36 +0200

    investigating mdc

--- service/src/main/java/com/heed/mp/helloworld/ErrorController.java
+++ service/src/main/java/com/heed/mp/helloworld/ErrorController.java
@@ -3,17 +3,20 @@ package com.heed.mp.helloworld;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/v1")
 public class ErrorController {
 
     @GetMapping("/error")
     public void error() {
-        throw new IllegalArgumentException("error");
+        throw new IllegalArgumentException(MDC.getCopyOfContextMap().toString());
     }
 
 }
